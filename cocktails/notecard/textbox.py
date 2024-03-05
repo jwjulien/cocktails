b32917db850893b85d737991b5699f8428104323
jared <jaredjulien@exsystems.net>
2024-03-04 19:22:03 -0500

    updated dates

--- cocktails/notecard/textbox.py
+++ cocktails/notecard/textbox.py
@@ -2,7 +2,7 @@
 #      File:  /notecard/textbox.py
 #   Project:  Recipes
 #    Author:  Jared Julien <jaredjulien@exsystems.net>
-# Copyright:  (c) 2023 Jared Julien, eX Systems
+# Copyright:  (c) 2023-2024 Jared Julien, eX Systems
 # ---------------------------------------------------------------------------------------------------------------------
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
